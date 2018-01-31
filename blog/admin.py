292b4f8cce553293464e3f077d4f8f8354918ca0
ShawnXu <iamxxh@qq.com>
2018-01-31 22:20:39 +0800

    Initial commit

---
+++ blog/admin.py
@@ -0,0 +1,7 @@
+# Register your models here.
+from django.contrib import admin
+from .models import Post, Category, Tag
+
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Tag)
